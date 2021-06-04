a481ab4dc800b96000b28b3ed6b8f20473ec0347
Euijae Lee <tenitlee@gmail.com>
2021-06-04 11:04:09 +0900

    App.js Second Change

--- App.js
+++ App.js
@@ -1,35 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, TouchableOpacity } from 'react-native';
+// after `expo install react-native-safe-area-context`.
+import { SafeAreaView } from 'react-native-safe-area-context';
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cities: [],
+    };
+  }
+  componentDidMount() {
+    fetch('https://raw.githubusercontent.com/example0312/weather-crawler/e3168f2b4e316691f8ab385f738783976eef7f0d/availableCityNames')
+      .then(response => response.json())
+      .then(cities => {
+        console.log('cities =', cities.length);
+        this.setState({
+          cities
+        });
+      });
+  }
+  onPressCity(item) {
+    console.log('onPressCity =', item);
+  }
+
+  renderItem(city) {
+    return (
+      <TouchableOpacity style={styles.item} onPress={() => this.onPressCity(city)}>
+        <Text style={styles.text}>{city}</Text>
+      </TouchableOpacity>
+    );
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.text}>1</Text>
-        <Text style={[styles.text, styles.text2]}>2</Text>
-        <Text style={styles.text}>3</Text>
+      <SafeAreaView style={styles.container}>
+        <FlatList style={styles.container}
+          keyExtractor={item => item}
+          renderItem={({ item }) => this.renderItem(item)}
+          data={this.state.cities}
+        />
         <StatusBar style="auto" />
-      </View>
+      </SafeAreaView>
     );
   }
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'stretch',
+  },
+  item: {
+    flex: 1,
+    height: 50,
     justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: 'orange',
   },
   text: {
-    flex: 1,
-    borderWidth: 1,
-    borderColor: 'red',
+    fontSize: 20,
     textAlign: 'center',
-    textAlignVertical: 'center',
-    fontSize: 24,
-    fontWeight: 'bold',
   },
-  text2: {
-    flex: 2,
-  }
 });
