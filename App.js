81ad81da708611b5979dd4ad832e6681481256d4
Euijae Lee <tenitlee@gmail.com>
2021-06-04 10:26:51 +0900

    App.js First Change

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { Button, StyleSheet, Text, View } from 'react-native';
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>1</Text>
+        <Text style={[styles.text, styles.text2]}>2</Text>
+        <Text style={styles.text}>3</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
+  text: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: 'red',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  text2: {
+    flex: 2,
+  }
 });
