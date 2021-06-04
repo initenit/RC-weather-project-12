55cb0bdc51739198b67e153d414ef61c54cb000e
Euijae Lee <tenitlee@gmail.com>
2021-06-04 12:09:01 +0900

    Final Commit

--- App.js
+++ App.js
@@ -1,62 +1,48 @@
+import * as React from 'react';
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { FlatList, StyleSheet, Text, TouchableOpacity } from 'react-native';
-// after `expo install react-native-safe-area-context`.
-import { SafeAreaView } from 'react-native-safe-area-context';
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cities: [],
-    };
-  }
-  componentDidMount() {
-    fetch('https://raw.githubusercontent.com/example0312/weather-crawler/e3168f2b4e316691f8ab385f738783976eef7f0d/availableCityNames')
-      .then(response => response.json())
-      .then(cities => {
-        console.log('cities =', cities.length);
-        this.setState({
-          cities
-        });
-      });
-  }
-  onPressCity(item) {
-    console.log('onPressCity =', item);
-  }
+import { View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import CityList from './CityList';
+import WeatherDetailScreen from './WeatherDetailScreen';
 
-  renderItem(city) {
-    return (
-      <TouchableOpacity style={styles.item} onPress={() => this.onPressCity(city)}>
-        <Text style={styles.text}>{city}</Text>
-      </TouchableOpacity>
-    );
-  }
-  render() {
-    return (
-      <SafeAreaView style={styles.container}>
-        <FlatList style={styles.container}
-          keyExtractor={item => item}
-          renderItem={({ item }) => this.renderItem(item)}
-          data={this.state.cities}
-        />
-        <StatusBar style="auto" />
-      </SafeAreaView>
-    );
-  }
-}
+const HomeScreen = ({ navigation }) => (
+  <View style={styles.container}>
+    <CityList navigation={navigation} />
+    <StatusBar style="auto" />
+  </View>
+);
+
+const DetailScreen = ({ navigation, route }) => (
+  <View style={styles.container}>
+    <WeatherDetailScreen navigation={navigation} route={route}/>
+    <StatusBar style="auto" />
+  </View>
+);
+const Stack = createStackNavigator();
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  item: {
-    flex: 1,
-    height: 50,
-    justifyContent: 'center',
-    borderWidth: 1,
-    borderColor: 'orange',
-  },
-  text: {
-    fontSize: 20,
-    textAlign: 'center',
-  },
 });
+export default class App extends React.Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ title: 'Cities' }}
+          />
+          <Stack.Screen
+            name="Detail"
+            component={DetailScreen}
+            options={{ title: 'Weather' }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
